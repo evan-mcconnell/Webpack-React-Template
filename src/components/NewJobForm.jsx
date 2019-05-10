f7eb063ce0a3906b4aff2ab2351469cb7cda49ce
Kimberly McConnell <kmcconnell393@gmail.com>
2019-05-10 11:47:02 -0700

    add form to newjobform component

--- src/components/NewJobForm.jsx
+++ src/components/NewJobForm.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
 function NewJobForm() {
+  let _title = null;
+  let _desciption = null;
+  let _lanes = null;
+
+  function handleNewJobSubmission(e) {
+    e.preventDefault();
+    console.log(_title.value);
+    console.log(_desciption.value);
+    console.log(_lanes.value);
+  }
+
   return (
-    <div></div>
+    <div>
+      <form onSubmit={handleNewJobSubmission}>
+        <input type='text' 
+          id='title'
+          placeholder='title'
+          ref={(input) => {_title = input;}} />
+        <input type='text' 
+          id='desciption'
+          placeholder='desciption'
+          ref={(input) => {_desciption = input;}} />
+        <input type='text' 
+          id='lanes'
+          placeholder='lanes'
+          ref={(input) => {_lanes = input;}} />
+        <button type='submit'>Add New Job</button>
+      </form>
+    </div>
   )
 }
 
